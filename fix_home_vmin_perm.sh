a16063e02bc1c0cab20abc5545313566cdb2f096
root <root@subd-lp2m.unmul.ac.id>
2024-07-28 17:21:56 +0800

    add fix permision default virtualmin

---
+++ fix_home_vmin_perm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+chmod 755 awstats
+chmod 755 bin
+chmod 750 cgi-bin
+chmod 755 cgi-bin/awstats.pl
+chmod 755 etc
+chmod 755 fcgi-bin
+chmod 755 homes
+chmod 750 logs
+chmod 700 Maildir
+chmod 700 Maildir/.Drafts
+chmod 700 Maildir/.Sent
+chmod 700 Maildir/.Trash
+chmod 700 Maildir/cur
+chmod 700 Maildir/new
+chmod 700 Maildir/tmp
+chmod 644 Maildir/subscriptions
+chmod 750 tmp
+chmod 700 virtualmin-backup
